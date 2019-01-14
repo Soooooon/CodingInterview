99429f3ed4d2d15a2a546744e7d108b57ae13b32
Leon Song <songliu0608@qq.com>
2019-01-14 17:07:15 +0800

    NO.11 find min

---
+++ src/code/NO11_FindMinTest.java
@@ -0,0 +1,35 @@
+package code;
+/**
+ * 面试题11：旋转数组的最小数字
+ * leetcode：153
+ * @author Soon
+ *
+ */
+public class NO11_FindMinTest {
+	public static void main(String[] args) {
+		//
+		int[] nums= {1};
+		//
+		System.out.println(findMin(nums));
+		
+	}
+	public static int findMin(int[] nums) {
+		int start=0;
+		int end=nums.length-1;
+		int mid=start;
+		
+		while(nums[start]>=nums[end]) {
+			if (end-start<=1) {
+				mid=end;
+				break;
+			}
+			mid=(start+end)/2;
+			if (nums[mid]>=nums[start]) {
+				start=mid;
+			} else {
+				end=mid;
+			}
+		}
+		return nums[mid];
+	}
+}
